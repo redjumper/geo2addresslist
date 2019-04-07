7a55c206f27a600cd9667048f98e6668193595e3
redjumper <vmshare@gmail.com>
2019-04-07 22:03:22 +0800

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 git remote add upstream "https://${geo_token}@github.com/redjumper/geo2addresslist"
 
 python geo2addresslist.py
-
+rm GeoLite2-ASN-Blocks-IPv4.csv
 #git checkout -b master
 git add -A
 git commit -m 'Update'
